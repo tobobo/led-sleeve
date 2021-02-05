35b90e2c70220b5cc81a04bc719b7f89db56d61b
Tobias Butler <tobias@change.org>
2021-02-04 16:47:39 -0800

    use imagemagick for image conversion

--- lib/image_preparer.py
+++ lib/image_preparer.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen, urlretrieve
 import mimetypes
 import logging
 import os
+from lib.stream_command import stream_with_labeled_output
 
 
 class ImagePreparer():
@@ -17,6 +18,24 @@ class ImagePreparer():
             if ext == ".jpe":
                 ext = ".jpg"
             image_path = "{}/album{}".format(self._image_dir_path, ext)
+            formatted_image_path = "{}/edited-album.png".format(self._image_dir_path)
             urlretrieve(image_url, image_path)
+            await stream_with_labeled_output(
+                "imagemagick",
+                [
+                    "/usr/bin/convert",
+                    image_path,
+                    "-resize", "64x64^",
+                    "-gravity", "center",
+                    "-extent", "64x64",
+                    # "-filter", "Hanning",
+                    # "-modulate", "100,110,100",
+                    "-gamma", "1.5",
+                    # "-contrast", "1.1",
+                    "-colorspace", "RGB",
+                    formatted_image_path
+                ]
+            )
+            image_path = formatted_image_path
             logging.debug(f"sending {image_path} to display proc")
             return image_path
